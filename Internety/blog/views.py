6540d311b09e0764018b3cc8736dcec26577b1ae
Michał Słowikowski <slovik421@gmail.com>
2019-03-24 20:09:47 +0100

    Comments

--- Internety/blog/views.py
+++ Internety/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 
 class PostListView(ListView):
@@ -11,14 +11,34 @@ class PostListView(ListView):
     template_name = 'blog/post/list.html' 
 
 def post_detail(request, year, month, day, post):
+    print(post)
     post = get_object_or_404(Post, slug=post,
                              status='published',
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    print(post.slug)
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    print(post.slug)
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status='published')
