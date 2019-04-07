d8ebfbc3d82765d3d00da0e9dc5916e8b1ecf800
Michał Słowikowski <slovik421@gmail.com>
2019-04-07 22:33:50 +0200

    Tags & sidebar

--- Internety/blog/urls.py
+++ Internety/blog/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path(r'', views.PostListView.as_view(), name='post_list'),
+    path(r'', views.post_list, name='post_list'), 
+    #path(r'', views.PostListView.as_view(), name='post_list'),
+    path('tag/<slug:tag_slug>/',     
+         views.post_list, name='post_list_by_tag'),
     path(r'<int:year>/<int:month>/<int:day>/<slug:post>',
          views.post_detail,
          name='post_detail'),
