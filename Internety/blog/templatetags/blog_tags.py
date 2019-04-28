5d1d09e78dd9fa30dfbe787b07840a9866d37758
Michał Słowikowski <slovik421@gmail.com>
2019-04-28 23:34:30 +0200

    Revert "Revert "Final""
    This reverts commit 2b81ea7693c4d2f5c193f039fc418ef95679ff1c.

--- Internety/blog/templatetags/blog_tags.py
+++ Internety/blog/templatetags/blog_tags.py
@@ -22,6 +22,11 @@ def get_most_commented_posts(count=5):
         total_comments=Count('comments')           
         ).order_by('-total_comments')[:count]
 
+@register.simple_tag
+def get_latest_posts(count=5):    
+    return Post.published.order_by('-publish')[:count] 
+
+
 @register.filter(name='markdown') 
 def markdown_format(text):    
     return mark_safe(markdown.markdown(text))
